615ecab0d83348a09cb7ee645df82a014fe28f56
paulchainkang <paul@paul-kang.com>
2022-01-24 22:23:02 +0800

    wip

--- D1_Mini_RFID_Reader/D1_Mini_RFID_Reader.ino
+++ D1_Mini_RFID_Reader/D1_Mini_RFID_Reader.ino
@@ -4,7 +4,6 @@
 #include <ESP8266WiFiMulti.h>
 
 #include <ESP8266HTTPClient.h>
-
 #include <WiFiClient.h>
 
 #include <SPI.h>
@@ -12,11 +11,57 @@
 #include <MFRC522.h>
 #define RST_PIN         D0 //D3       // Configurable, see typical pin layout above
 #define SS_PIN          D8 //D4       // Configurable, see typical pin layout above
-MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
+MFRC522 mfrc522(SS_PIN, RST_PIN);     // Create MFRC522 instance
+
+#define blueLED   3 //RX
+#define greenLED D1
+#define redLED   D2 // same as beep
+#define beepPin  D2
 
 ESP8266WiFiMulti WiFiMulti;
 
+long times=0;
+WiFiClient client;
+HTTPClient http;
+
+#define MAC
+//#define abc
+//#define TCRD4G
+
+#if defined(MAC)
+const char* ssid = "MAC";
+const char* password = "Kpc24642464";
+#endif
+#if defined(abc)
+const char* ssid = "abc";
+const char* password = "12345678";
+#endif
+#if defined(TCRD4G)
+const char* ssid = "TCRD4G";
+const char* password = "cOUWUnWPU1";
+#endif
+
+#define jigsaw
+#define heroku
+
+#if defined(jigsaw)
+const char* apiURL = "http://jigsaw.w3.org/HTTP/connection.html";
+#endif
+#if defined(heroku)
+const char* apiURL = "http://charder-weight-api.herokuapp.com/";
+#endif
+
 void setup() {
+
+  pinMode(beepPin, OUTPUT); 
+
+  // beep--- beep beep
+  digitalWrite(beepPin, HIGH);   delay(300);
+  digitalWrite(beepPin, LOW);    delay(100);
+  digitalWrite(beepPin, HIGH);   delay(100);
+  digitalWrite(beepPin, LOW);    delay(100);
+  digitalWrite(beepPin, HIGH);   delay(100);
+  digitalWrite(beepPin, LOW);  
 
   Serial.begin(115200);
   // Serial.setDebugOutput(true);
@@ -29,92 +74,125 @@ void setup() {
 
   SPI.begin();
   mfrc522.PCD_Init();   // Init MFRC522
-  delay(4);       // Optional delay. Some board do need more time after init to be ready, see Readme
-  mfrc522.PCD_DumpVersionToSerial();  // Show details of PCD - MFRC522 Card Reader details
-  Serial.println(F("Scan PICC to see UID, SAK, type, and data blocks..."));     
+  delay(4);             // Optional delay. Some board do need more time after init to be ready, see Readme
+  
+//  WiFi.mode(WIFI_STA);
 
-  for (uint8_t t = 4; t > 0; t--) {
-    Serial.printf("[SETUP] WAIT %d...\n", t);
-    Serial.flush();
-    delay(1000);
+  WiFi.begin(ssid, password);
+
+  Serial.printf("Connecting to AP/Router %s...\n", ssid); 
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected! IP address: ");
+  Serial.println(WiFi.localIP());
+
+  // 用 iPhone AP 或 TCRD4G 都沒問題，
+  // 但用家裡 TP-LINK AP，第一次呼叫 API 總是失敗，所以這裡做 retry
+  for (int i=0; i<3; i++){
+    if(apiHttpGet(apiURL)) 
+      break;
+    else 
+      Serial.printf("Calling API failed %d times\n", (i+1));
   }
 
-  WiFi.mode(WIFI_STA);
-  WiFiMulti.addAP("TCRD4G", "cOUWUnWPU1");
-  WiFiMulti.addAP("abc", "12345678");  
-
+  Serial.println(F("Scan PICC to see UID, SAK, type, and data blocks..."));  
 }
 
-long times=0;
 void loop() {
-  // *****
-  // re-init for cheap RFID cards
-  mfrc522.PCD_Init();
-  delay(4);  
-  // *****
+  
+  if (WiFi.status() == WL_CONNECTED) {
     
-  // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
-  if ( ! mfrc522.PICC_IsNewCardPresent()) {
-
-    
-    // Read A0
-    //analogValue=analogRead(A0);
-//    if (analogValue <1000){
-//      Serial.println(analogRead(A0));   
-//      return;
-//    }
-       
-  } else {
-    // Select one of the cards
-    if ( ! mfrc522.PICC_ReadCardSerial()) {
-      // Read A0
-      Serial.println(analogRead(A0));      
-      return;
-    }
-
-    // Dump debug info about the card; PICC_HaltA() is automatically called
-    mfrc522.PICC_DumpToSerial(&(mfrc522.uid)); 
-
-    Serial.println("******** times *********");
-    Serial.println(++times);
-    Serial.println("******** times *********");
-            
-    // wait for WiFi connection
-    if ((WiFiMulti.run() == WL_CONNECTED)) {
-  
-      WiFiClient client;
-  
-      HTTPClient http;
-  
-      Serial.print("[HTTP] begin...\n");
-      //if (http.begin(client, "http://jigsaw.w3.org/HTTP/connection.html")) {  // HTTP
-      if (http.begin(client, "http://charder-weight-api.herokuapp.com/")) {  // HTTP
-  
-  
-        Serial.print("[HTTP] GET...\n");
-        // start connection and send HTTP header
-        int httpCode = http.GET();
-  
-        // httpCode will be negative on error
-        if (httpCode > 0) {
-          // HTTP header has been send and Server response header has been handled
-          Serial.printf("[HTTP] GET... code: %d\n", httpCode);
-  
-          // file found at server
-          if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
-            String payload = http.getString();
-            Serial.println(payload);
-          }
-        } else {
-          Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
-        }
-  
-        http.end();
-      } else {
-        Serial.printf("[HTTP} Unable to connect\n");
+    // re-init for cheap RFID cards
+    mfrc522.PCD_Init();
+    delay(4);  
+      
+    // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
+    if ( ! mfrc522.PICC_IsNewCardPresent()) {
+        
+    } else {
+      // Select one of the cards
+      if ( ! mfrc522.PICC_ReadCardSerial()) {
+        // Read A0
+        // Serial.println(analogRead(A0));      
+        return;
       }
+  
+      // short beep
+      digitalWrite(beepPin, HIGH);   
+      delay(100);
+      digitalWrite(beepPin, LOW);
+      
+      // Dump debug info about the card; PICC_HaltA() is automatically called
+      //mfrc522.PICC_DumpToSerial(&(mfrc522.uid)); 
+  
+      //Serial.println("******** times *********");
+      //Serial.println(++times);
+      //Serial.println("******** times *********");
+  
+      // 顯示卡片內容
+      Serial.print(F("Card UID:"));
+      dump_byte_array(mfrc522.uid.uidByte, mfrc522.uid.size); // 顯示卡片的UID
+      Serial.println();
+      Serial.print(F("PICC type: "));
+      MFRC522::PICC_Type piccType = mfrc522.PICC_GetType(mfrc522.uid.sak);
+      Serial.println(mfrc522.PICC_GetTypeName(piccType));  //顯示卡片的類型
+  
+      mfrc522.PICC_HaltA();  // 卡片進入停止模式
+
+      // 用 iPhone AP 或 TCRD4G 都沒問題，
+      // 但用家裡 TP-LINK AP，API 呼叫 API 有時會失敗，所以這裡做 retry      
+      for (int i=0; i<3; i++){
+        if(apiHttpGet(apiURL)) 
+          break;
+        else 
+          Serial.printf("Calling API failed %d times\n", (i+1));
+      }
+              
+      Serial.println(F("Scan PICC to see UID, SAK, type, and data blocks..."));      
+      delay(500);
     }
+
+  } else {
+    Serial.println("Networked is not connected");
+    delay(1000);
   }
-    
-  //delay(30000);
+}
+
+bool apiHttpGet(const char* apiURL){
+  bool success=false;
+  
+  Serial.print("[HTTP] begin...\n");
+  if (http.begin(client, apiURL)) {  // HTTP
+
+    Serial.print("[HTTP] GET...\n");
+    // start connection and send HTTP header
+    int httpCode = http.GET();
+
+    // httpCode will be negative on error
+    if (httpCode > 0) {
+      // HTTP header has been send and Server response header has been handled
+      Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+
+      // file found at server
+      if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
+        String payload = http.getString();
+        Serial.println(payload);
+        success = true;
+      }
+    } else {
+      Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+    }
+
+    http.end();
+    return success;
+  }  
+}
+void dump_byte_array(byte *buffer, byte bufferSize) {
+  for (byte i = 0; i < bufferSize; i++) {
+    Serial.print(buffer[i] < 0x10 ? " 0" : " ");
+    Serial.print(buffer[i], HEX);
+  }
 }
